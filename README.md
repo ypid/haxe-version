b8fc83d035f0075f8dad53a462aff667097fb3af
Robin Schneider <ypid@riseup.net>
2015-12-05 22:59:16 +0100

    Improved documentation.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Haxe macros useful for including version strings (version from `haxelib.json`, git commit hash and Haxe version) to a target build.
 
-See [Version.hx](https://github.com/ypid/haxe-version/blob/master/Version.hx) for details.
+To use it, just install it via `haxelib install version` and use the `-lib version` switch for `haxe`.
+
+Refer to [Version.hx](https://github.com/ypid/haxe-version/blob/master/Version.hx) to see which macros are available and how to use them.
 
 Credits for the idea go to [Andy Li](http://blog.onthewings.net/) who presented a similar macro using `git describe --tags` in [Haxe, a language that compiles to JS](http://blog.onthewings.net/about/).
--- Version.hx
+++ Version.hx
@@ -2,8 +2,8 @@ class Version {
     /**
         Return version as defined in the `haxelib.json` file. Example usage:
 
-            Version of the suncalc library.
-            public static var version(default, never):String = getVersion();
+            Version of the library.
+            public static var version(default, never):String = Version.getVersion();
     **/
     public static macro function getVersion(filename:String = 'haxelib.json'):haxe.macro.Expr {
         var haxelib_json = haxe.Json.parse(sys.io.File.getContent(filename));
@@ -14,7 +14,7 @@ class Version {
         Return the latest git commit hash. Example usage:
 
             git commit hash from which this library was build.
-            public static var version_hash(default, never):String = getGitCommitHash();
+            public static var version_hash(default, never):String = Version.getGitCommitHash();
     **/
     public static macro function getGitCommitHash():haxe.macro.Expr {
         var git_rev_parse_HEAD = new sys.io.Process('git', [ 'rev-parse', 'HEAD' ] );
@@ -29,7 +29,7 @@ class Version {
         Return the version of Haxe. Example usage:
 
             Version of Haxe used to build this library.
-            public static var version_haxe_compiler(default, never):String = getHaxeCompilerVersion();
+            public static var version_haxe_compiler(default, never):String = Version.getHaxeCompilerVersion();
     **/
     public static macro function getHaxeCompilerVersion():haxe.macro.Expr {
         // Returns float number like: 3.201
